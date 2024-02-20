d7eed8a7e360408fb089ed9632312611bb232aea
sepezeus <sericomput@gmail.com>
2024-02-20 22:13:37 +0200

    Basic weather API functional

---
+++ backend/routes/apis/userAPI.js
@@ -0,0 +1,21 @@
+const USER_LOCATION = 'https://api.ipregistry.co/?key=d9tj0m8blqg53opj&pretty=true'
+
+const fetchUserLatLong = async () =>{
+    try {
+        //return {latitude: null, longitude: null}; //temporary testing purposes
+        const response = await fetch(USER_LOCATION);
+        const contentType = response.headers.get("content-type");
+        if (!contentType) {
+          throw new TypeError("Could not retrieve user location data");
+        }
+        const jsonData = await response.json();
+        const latitude = jsonData["location"].latitude;
+        const longitude = jsonData["location"].longitude;
+
+        console.log(longitude, latitude);
+        return {latitude: latitude, longitude: longitude};
+      } catch (error) {
+        console.error("Error:", error);
+      }
+}
+export {fetchUserLatLong}
---
+++ backend/src/deps.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import path from 'path';
+
+export {express, path};
