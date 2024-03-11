382f96bcbcfdaab1f929d4f4af14cd377f4c0494
Tatugh <tatu.ruohoaho@hotmail.fi>
2024-03-11 19:06:55 +0200

    Lämpötila tulostetaan kokoviikon ajalta

--- frontend/src/components/DailyWeather.jsx
+++ frontend/src/components/DailyWeather.jsx
@@ -1,38 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const DailyWeather = () => {
-  return (
-    <div className='weatherTable'>
-        <div className='weatherItems'>
-            <h3>Ma</h3>
-            <p>4°C</p>
+    const [date, setDate] = useState([])
+    const [tempMax, setTempMax] = useState([])
+    const [tempMin, setTempMin] = useState([])
+    const [tempAvg, setTempAvg] = useState([])
+    
+    
+    useEffect(() => {
+        const check = async () => {
+            try {
+                const response = await axios.get("http://localhost:3001/api/weather/daily")
+                const responseResult = await response.data.daily
+                setDate(responseResult.time)
+                setTempMax(responseResult.temperature_2m_max)
+                setTempMin(responseResult.temperature_2m_min)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        check()
+
+        //temperatureAverage()
+    }, [])
+    
+
+    // const calculateAverage = (a, b) => {
+    //     return Math.ceil((a + b) / 2)
+    // }
+
+    // const temperatureAverage = () => {
+    //     let arr = []
+    //     for (let i = 0; i < 7; i++) {
+    //         arr.push(calculateAverage(tempMin[i], tempMax[i]))
+    //     }
+    //     setTempAvg(arr)
+    // }
+
+    return (
+        <div className='weatherTable'>
+            {date.map((item, index) => {
+                return (
+                    <div key={item} className='weatherItems'>
+                        <h3>{item.slice(5)}</h3>
+                        <p>{Math.round(tempMax[index])}°C</p>
+                    </div>
+                )
+            })}
         </div>
-        <div className='weatherItems'>
-            <h3>Ti</h3>
-            <p>4°C</p>
-        </div>
-        <div className='weatherItems'>
-            <h3>Ke</h3>
-            <p>4°C</p>
-        </div>
-        <div className='weatherItems'>
-            <h3>To</h3>
-            <p>4°C</p>
-        </div>
-        <div className='weatherItems'>
-            <h3>Pe</h3>
-            <p>4°C</p>
-        </div>
-        <div className='weatherItems'>
-            <h3>La</h3>
-            <p>4°C</p>
-        </div>
-        <div className='weatherItems'>
-            <h3>Su</h3>
-            <p>4°C</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default DailyWeather
