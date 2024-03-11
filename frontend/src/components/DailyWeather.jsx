577dd9a730f68775f5f355b852794a995b4208fb
Tatugh <tatu.ruohoaho@hotmail.fi>
2024-03-11 16:34:32 +0200

    componentti kansio sekä tiedosto

---
+++ frontend/src/components/DailyWeather.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const DailyWeather = () => {
+  return (
+    <div className='weatherTable'>
+        <div className='weatherItems'>
+            <h3>Ma</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>Ti</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>Ke</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>To</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>Pe</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>La</h3>
+            <p>4°C</p>
+        </div>
+        <div className='weatherItems'>
+            <h3>Su</h3>
+            <p>4°C</p>
+        </div>
+    </div>
+  )
+}
+
+export default DailyWeather
