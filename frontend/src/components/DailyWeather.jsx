bd90b8d216d70fdf46e853c5a7d1c713ba56f9b7
Tatugh <tatu.ruohoaho@hotmail.fi>
2024-03-12 12:56:10 +0200

    DailyWeather muutosta

--- frontend/src/components/DailyWeather.jsx
+++ frontend/src/components/DailyWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const DailyWeather = () => {
@@ -25,7 +25,9 @@ const DailyWeather = () => {
         //temperatureAverage()
     }, [])
     
-
+    const trimDate = () => {
+        let temporaryDate = date
+    }
     // const calculateAverage = (a, b) => {
     //     return Math.ceil((a + b) / 2)
     // }
