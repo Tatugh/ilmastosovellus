b7857cd7707d048399e55c74bbceb4711497a1f0
Janne Toivanen <ejato004@edu.xamk.fi>
2024-03-19 03:09:32 +0200

    testi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import * as weatherAPIHelper from './apis/WeatherAPIHelper.jsx'
+import Button from "react-bootstrap/Button";
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
       <div className="content-container">
-      <h1>Sähkö</h1>
+      <div className='location-container'>
+      <h1 className='current-location'>Mikkeli</h1>
+      <button className="btn-info">+</button>
+      </div>
       <h2>Current Weather Information:</h2>
       <div className="weather-container">
           <weatherAPIHelper.CurrentWeather/>
