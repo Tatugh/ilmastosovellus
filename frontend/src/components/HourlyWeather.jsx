37c67856f89123cba55aeba13430823ead74de2d
JHeget <dheja005@edu.xamk.fi>
2024-04-02 14:16:14 +0300

    chart updated

--- frontend/src/components/HourlyWeather.jsx
+++ frontend/src/components/HourlyWeather.jsx
@@ -1,12 +1,12 @@
 import  { useEffect, useState } from 'react'
-import { Line } from "react-chartjs-2"; 
+import { Line, Bar } from "react-chartjs-2"; 
 import {Chart as ChartJS} from "chart.js/auto";
 
 function HourlyWeather() {
     const [time, setTime] = useState([]);
     const [temperatures, setTemperatures] = useState([]);
     const [rain, setRain] = useState([]);
-
+  
     useEffect(() => {
         const fetchHourlyWeatherData = async () => {
             try {
@@ -15,59 +15,73 @@ function HourlyWeather() {
                   throw new Error('Network response error');
                 }
                 const curWeather = await response.json();
-                setTime(curWeather.hourly.time);
+                const formattedTime = curWeather.hourly.time.map(str => str.slice(11, str.length));
+                setTime(formattedTime);
                 setTemperatures(curWeather.hourly.temperature_2m);
                 setRain(curWeather.hourly.precipitation);
-                console.log(curWeather.hourly);
+                console.log(curWeather.hourly.time);
             } catch (error) {
             console.error('Error fetching hourly weather:', error);
             }
         };
         fetchHourlyWeatherData();
-    }, []);
+      }, []);
+          
 
     const chartOptions = {
-        color: "white",
-      
-        scales: {
+      color: "white",
+      scales: {
         x: {
           ticks: {
             color: 'white', // Change x-axis font color here
           },
         },
         y: {
+          position: "left",
           ticks: {
             color: 'white', // Change y-axis font color here
           },
         },
-      },
-    }
+        y1: {
+          position: "right",
+          ticks: {
+            color: 'white', 
+          },
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+      }
+    }  
 
     return <div className = "weatherChart">
-        <Line 
-            options={chartOptions}
-            // datasetIdKey='id'
-            data={{
-                labels: time,
-                datasets: [
-                { 
-                    id: 1,
-                    label: 'Temperature',
-                    data: temperatures,
-                    borderColor: 'red',
-                    backgroundColor: 'red',
-                },
-                { 
-                    id: 2,
-                    label: 'Precipitation',
-                    data: rain,
-                    borderColor: 'blue',
-                    backgroundColor: 'blue',
-                }
-                ],
-            }
-            }
-        />
+      <Line 
+        options={chartOptions}
+        // datasetIdKey='id'
+        data={{
+          labels: time,
+          datasets: [
+          { 
+            id: 1,
+            label: 'Temperature',
+            data: temperatures,
+            borderColor: '#ff4726',
+            backgroundColor: '#ff4726',
+            yAxisID: 'y',
+          },
+          { 
+            id: 2,
+            label: 'Precipitation',
+            data: rain,
+            borderColor: '#34b9f7',
+            backgroundColor: '#34b9f7',
+            type: "bar",
+            yAxisID: 'y1',
+          }
+          ],
+        }
+        }
+      />
     </div>
 }
 
