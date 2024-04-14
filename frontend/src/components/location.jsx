978c10fe4dafe8ff8699b1c14b809a06e6971558
Martin <martin.nipuli2@gmail.com>
2024-04-14 15:08:33 +0300

    Location data fetch and save to local storage

--- frontend/src/components/location.jsx
+++ frontend/src/components/location.jsx
@@ -8,9 +8,45 @@ import Dropdown from "react-bootstrap/Dropdown";
 
 const LocationDisplay = () => {
     const [show, setShow] = useState(false);
+    const [query, setQuery] = useState("")
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    // Constructs new 'locData' array which contains 5 locations as objects from fetched data
+    // Then sets the data as valid JSON string to localStrorage with key of 'locationData'
+    // When you get 'locationData' from localStorage you have to parse the JSON string data with builtin function
+    // 'JSON.parse()'. This returns a valid JSON object which then can be used for later purposes
+    const setLocalStorage = (data) => {
+        const locData = data.results.map((location) => {
+            return {
+                name: location.name,
+                longitude: location.longitude,
+                latitude: location.latitude
+            }
+        })
+
+        localStorage.setItem("locationData", JSON.stringify(locData))
+    }
+
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()    
+    
+        fetch(`http://localhost:3001/api/location/data?q=${query}`, {
+            method: "POST"
+        }).then((res) => {
+            return res.json()  
+        }).then((data) => {
+            setLocalStorage(data)
+        }).catch((err) => {
+            console.error(err)
+        })
+    }
+    
     return (
     <div className='location-container'>        
           <button className='btn-add' onClick={handleShow}></button>
@@ -19,13 +55,13 @@ const LocationDisplay = () => {
                 <Modal.Title className="modal-title">Add a location</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Control size="lg" type="text" placeholder="Narnia" />
+                <Form onSubmit={handleSubmit}>
+                    <Form.Control size="lg" type="text" placeholder="Narnia" onChange={handleChange}/>
+                    <Modal.Footer>
+                        <button type="submit" className="btn btn-primary" onClick={handleClose}>Search</button>
+                    </Modal.Footer>
                 </Form>
             </Modal.Body>
-            <Modal.Footer>
-                <button type="button" className="btn btn-primary" onClick={handleClose}>OK</button>
-            </Modal.Footer>
           </Modal>
           
           <h1 className='current-location'>Mikkeli</h1>
