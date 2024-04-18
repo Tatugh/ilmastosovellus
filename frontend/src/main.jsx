2ab9100e4fc81f490f09fd2dcc39343442c94132
sepezeus <sericomput@gmail.com>
2024-04-18 20:43:17 +0300

    Brought back React

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import React, { useState } from "react";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
